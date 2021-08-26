c1c8695150ea853af0aaa764f5a098b688831f19
Joeri Bakker <bakkerjoeri@gmail.com>
2021-08-26 13:42:59 +0200

    Fix build folder

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -3,6 +3,9 @@ export default {
     public: '/',
     src: '/dist',
   },
+  buildOptions: {
+    out: 'dist'
+  },
   plugins: [
     '@snowpack/plugin-svelte',
     '@snowpack/plugin-typescript',
